ffc4de278f7ee97495dfe7b43d534b245856cd1e
eloilsondosanjos <eloilson.ageedu@gmail.com>
2021-07-04 10:26:57 -0300

    Refatorado implementação de criação de jobs

--- src/model/Job.js
+++ src/model/Job.js
@@ -26,11 +26,13 @@ module.exports = {
   get() {
     return data;
   },
-
   update(newJob) {
     data = newJob;
   },
   delete(id) {
     data = data.filter((job) => Number(job.id) !== Number(id));
+  },
+  create(newJob) {
+    data.push(newJob);
   },
 };
